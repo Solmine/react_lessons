75b764693698c397b2b6c21c345f048fbb33217a
Solmine <cosmasullivan@gmail.com>
2022-01-14 04:56:04 +0200

    create components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import './App.css';
+import Users from "./components/Users";
+import Posts from "./components/Posts";
+import Comments from "./components/Comments";
 
 const App = () => {
     return (
         <>
-
+            <div className={'wrapper'}>
+                <Users/>
+                <Posts/>
+            </div>
+            <Comments/>
         </>
     );
 };
---
+++ src/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Comments = () => {
+    return (
+        <>
+            <p>Comments</p>
+        </>
+    );
+};
+
+export default Comments;
